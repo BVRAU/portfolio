e90c1505f36f0995d9c01d7bae13161715133801
BVR <15015573+BVRAU@users.noreply.github.com>
2025-02-10 20:14:07 +1100

    Additions for practice

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <Section className="light" id="education">
+            <h2 style={{ textAlign: "center" }}>Education</h2>
+            <div className="education">
+                <div>
+                    <h3>University</h3>
+                    <p>Bachelor</p>
+                </div>
+            </div>
+        </Section>
+    )
+}
+
+export default Education;
