80a50bdf66210df7c08e6e6711b4a76397d8dd06
BVR <15015573+BVRAU@users.noreply.github.com>
2025-02-11 16:36:15 +1100

    Further updates

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -55,7 +55,8 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideInLeft"}}
+            className="slide-in-left"
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
             alt={imageAltText}
           />
         </div>
